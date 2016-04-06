06957f304537af1366116be08a3073c3c96fdab2
Levi Adair <laa024@bucknell.edu>
2016-04-06 10:45:38 -0400

    model package

---
+++ src/Model/GameBoard.java
@@ -0,0 +1,24 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2016 *
+ * Name: Patrick Newhart, Levi Adair, Sam Greenberg, Tom Ficcadenti
+ * Date: Apr 6, 2016
+ * Time: 10:37:14 AM *
+ * Project: csci205FinalProject
+ * Package: Model
+ * File: GameBoard
+ * Description:
+ *
+ * **************************************** */
+package Model;
+
+/**
+ *
+ * @author laa024
+ */
+public interface GameBoard {
+
+    final static int BOARD_HEIGHT = 16;
+    final static int BORAD_WIDTH = 14;
+
+}
