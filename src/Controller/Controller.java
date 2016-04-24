c05db8470be1d016929cfb74f494adaba27d1a90
Tom Ficcadenti <tjf010@bucknell.edu>
2016-04-24 17:52:32 -0400

    GUI stuff

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -37,7 +37,7 @@ public class Controller implements ActionListener, ChangeListener {
     }
 
     @Override
-    public void keyPressed(keyEvent e) {
+    public void keyPressed(KeyEvent e) {
 
     }
 }
