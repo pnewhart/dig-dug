2fd3ab355dfc19bbef0aa455186355e57e77ebc0
Levi Adair <laa024@bucknell.edu>
2016-04-27 18:51:33 -0400

    work on trying to make it work

---
+++ src/Model/modelTest.java
@@ -0,0 +1,23 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2016 *
+ * Name: Patrick Newhart, Levi Adair, Sam Greenberg, Tom Ficcadenti
+ * Date: Apr 13, 2016
+ * Time: 10:17:24 AM *
+ * Project: csci205FinalProject
+ * Package: Model
+ * File: modelTest
+ * Description:
+ *
+ * **************************************** */
+package Model;
+
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author laa024
+ */
+public class modelTest extends JComponent {
+
+}
